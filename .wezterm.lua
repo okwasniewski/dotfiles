ec302f7a509f4f81972ccc29dde03fa512f2f348
Oskar Kwaśniewski <oskarkwasniewski@icloud.com>
2024-09-13 13:13:42 +0200

    feat: add transparency

--- .wezterm.lua
+++ .wezterm.lua
@@ -4,6 +4,9 @@ local config = wezterm.config_builder()
 
 config.color_scheme = "catppuccin-mocha"
 config.font_size = 14.5
+config.window_background_opacity = 0.95
+config.macos_window_background_blur = 25
+config.window_decorations = "RESIZE"
 
 config.enable_tab_bar = false
 config.enable_scroll_bar = false
