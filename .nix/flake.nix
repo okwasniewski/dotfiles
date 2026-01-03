98a38b181fd00d5a27d367e3240475b8a6a8890c
Oskar Kwaśniewski <oskarkwasniewski@icloud.com>
2026-01-03 12:41:46 +0100

    feat: move to hosts file in nix

--- .nix/flake.nix
+++ .nix/flake.nix
@@ -5,122 +5,18 @@
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
-  let
-    configuration = { pkgs, ... }: {
-      environment.systemPackages = with pkgs; [
-        bat
-        cmake
-        coreutils
-        curl
-        direnv
-        eza
-        btop
-        lazydocker
-        ffmpeg
-        fzf
-        git
-        delta
-        lazygit
-        neovim
-        ninja
-        nmap
-        nodejs
-        ripgrep
-        ruby
-        scrcpy
-        starship
-        tree
-        watchman
-        yarn
-        zoxide
-      ];
-
-      homebrew = {
-        enable = true;
-        onActivation = {
-          autoUpdate = true;
-          cleanup = "zap";
-        };
-        taps = [
-          "xcodesorg/made"
-        ];
-        brews = [
-          "ccache"
-          "cocoapods"
-          "folly"
-          "nvm"
-          "python@3.9"
-          "rbenv"
-          "swiftlint"
-          "tmux"
-          "xcode-build-server"
-          "opencode"
-          "pnpm"
-        ];
-        casks = [
-          "1password"
-          "balenaetcher"
-          "android-platform-tools"
-          "google-chrome"
-          "cleanshot"
-          "font-fira-code-nerd-font"
-          "fork"
-          "ghostty"
-          "minisim"
-          "obsidian"
-          "raycast"
-          "spotify"
-          "xcodes-app"
-        ];
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      nix-darwin,
+      ...
+    }:
+    {
+      darwinConfigurations.default = nix-darwin.lib.darwinSystem {
+        system = "aarch64-darwin";
+        specialArgs = { inherit inputs self; };
+        modules = [ ./hosts/darwin/configuration.nix ];
       };
-
-      programs.zsh = {
-        enable = true;
-        enableCompletion = true;
-        enableBashCompletion = true;
-      };
-
-      environment.variables = {
-        ANDROID_HOME = "$HOME/Library/Android/sdk";
-        LANG = "en_US.UTF-8";
-        EDITOR = "nvim";
-        VISUAL = "nvim";
-        REACT_EDITOR = "nvim";
-      };
-
-      nix.settings.experimental-features = "nix-command flakes";
-
-      system.defaults = {
-        dock.tilesize = 30;
-        dock.magnification = true;
-        dock.largesize = 60;
-        dock.show-process-indicators = true;
-        dock.mru-spaces = false;
-        dock.autohide = true;
-
-        finder.AppleShowAllExtensions = true;
-        finder.FXEnableExtensionChangeWarning = false;
-        finder.FXPreferredViewStyle = "Nlsv";
-        finder.ShowPathbar = true;
-
-        NSGlobalDomain.AppleScrollerPagingBehavior = true;
-        NSGlobalDomain.NSTableViewDefaultSizeMode = 2;
-        NSGlobalDomain.KeyRepeat = 2;
-        NSGlobalDomain.InitialKeyRepeat = 15;
-
-        WindowManager.EnableStandardClickToShowDesktop = false;
-      };
-
-      system.primaryUser = "okwasniewski";
-      system.configurationRevision = self.rev or self.dirtyRev or null;
-      system.stateVersion = 6;
-      nixpkgs.hostPlatform = "aarch64-darwin";
     };
-  in
-  {
-    darwinConfigurations."default" = nix-darwin.lib.darwinSystem {
-      modules = [ configuration ];
-    };
-  };
 }
